a91aff7d848d20ed36d51b1fc8740c4bd500868e
Chris Chalkias <christos.nvsf@gmail.com>
2024-11-20 13:00:25 +0200

    storing additional data into db

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -103,7 +103,7 @@
   </vee-form>
 </template>
 <script>
-import firebase from '@/includes/firebase'
+import { auth, userCollection } from '@/includes/firebase'
 export default {
   name: 'RegisterForm',
   data() {
@@ -136,9 +136,20 @@ export default {
 
       let userCred = null
       try {
-        userCred = await firebase
-          .auth()
-          .createUserWithEmailAndPassword(values.email, values.password)
+        userCred = await auth.createUserWithEmailAndPassword(values.email, values.password)
+      } catch (error) {
+        this.reg_in_submission = false
+        this.reg_alert_variant = 'bg-red-500'
+        this.reg_alert_msg = 'An unexpected error occured. Please try again later.'
+        return
+      }
+      try {
+        await userCollection.add({
+          name: values.name,
+          email: values.email,
+          age: values.age,
+          country: values.country,
+        })
       } catch (error) {
         this.reg_in_submission = false
         this.reg_alert_variant = 'bg-red-500'
--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCPJfjWXETfKlUD4A5BuAjDaJlpGHOc0Ek',
@@ -8,4 +9,11 @@ const firebaseConfig = {
   storageBucket: 'music-97487.firebasestorage.app',
   appId: '1:900124028886:web:b19a6216569e9db5387e77',
 }
-export default firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig)
+
+const auth = firebase.auth()
+const db = firebase.firestore()
+
+const userCollection = db.collection('users')
+
+export { auth, db, userCollection }
